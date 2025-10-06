ed7692156086cbdeb3f130fcc082b28fed6ad8b4
ggnado <luigimassa2005@gmail.com>
2025-10-06 15:16:54 +0200

    Update swagger

--- src/main/java/org/elis/socialnetwork/swagger/Swagger.java
+++ src/main/java/org/elis/socialnetwork/swagger/Swagger.java
@@ -1,9 +1,12 @@
 package org.elis.socialnetwork.swagger;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,12 +19,12 @@ public class Swagger {
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
-//                .addSecurityItem(
-//                        new SecurityRequirement().addList("Bearer Authentication")
-//                )
-//                .components(
-//                        new Components().addSecuritySchemes("Bearer Authentication", createAPIKeyScheme())
-//                )
+                .addSecurityItem(
+                        new SecurityRequirement().addList("Bearer Authentication")
+                )
+                .components(
+                        new Components().addSecuritySchemes("Bearer Authentication", createAPIKeyScheme())
+                )
                 .info(
                         new Info()
                                 .title("SocialNetwork API")
@@ -59,10 +62,10 @@ public class Swagger {
                 ));
     }
 
-//    private SecurityScheme createAPIKeyScheme() {
-//        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
-//                .bearerFormat("JWT")
-//                .scheme("bearer");
-//    }
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
+    }
 
 }
